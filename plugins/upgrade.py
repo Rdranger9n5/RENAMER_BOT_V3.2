2d044858aa16d15bf7e1c03ded1915e385f7664f
Rdranger9n5 <145197281+Rdranger9n5@users.noreply.github.com>
2024-04-15 20:55:12 -0700

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -17,14 +17,14 @@ async def upgrade(bot,update):
 	Price Rs  2051  🇮🇳/🌎 25$  per Year
 	
 	
-	Pay Using Upi I'd ```9480251952@paytm```
+	@Xdexoy```
 	
 	After Payment Send Screenshots Of 
         Payment To Admin"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mrlokaman")], 
-        			[InlineKeyboardButton("PayPal 🌎",url = "https://www.paypal.me/lokamanchendekar"),
-        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/los89jy0")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/Xdexoy")], 
+        			[InlineKeyboardButton("PayPal 🌎",url = "https://www.paypal.me/"),
+        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
@@ -43,12 +43,12 @@ async def upgradecm(bot,message):
 	Price Rs  2051  🇮🇳/🌎 25$  per Year
 	
 	
-	Pay Using Upi I'd ```9480251952@paytm```
+	@Xdexoy```
 	
 	After Payment Send Screenshots Of 
         Payment To Admin"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mrlokaman")], 
-        			[InlineKeyboardButton("PayPal 🌎",url = "https://www.paypal.me/lokamanchendekar"),
-        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/los89jy0")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/Xdexoy")], 
+        			[InlineKeyboardButton("PayPal 🌎",url = "https://www.paypal.me/"),
+        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/Xdexoy")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await message.reply_text(text = text,reply_markup = keybord)
